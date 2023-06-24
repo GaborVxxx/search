45d7e9c952178ce7e360706a524a30fac6039f7a
GaborVxxx <gaborvtattoo@gmail.com>
2023-06-24 16:30:33 +0100

    CI/CD failed on eslint. Fixed.

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ import 'semantic-ui-less/semantic.less'
 // Redux
 import { Provider } from 'react-redux'
 import { store } from './app/store'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
+//import { PersistGate } from 'redux-persist/integration/react'
+//import { persistStore } from 'redux-persist'
 
 //let persistor = persistStore(store)
 
